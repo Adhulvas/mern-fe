56685469ef52b88d7acbf59f48ff54a70ab82867
Adhulvas <adhulvas2000@gmail.com>
2025-01-10 22:46:21 +0530

    navigation issue solved

--- src/Auth.js
+++ src/Auth.js
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from "react-router-dom";
 export function Auth(){
   const token = localStorage.getItem('token')
   return(
-    token?<Outlet/>:<Navigate to='/'/>
+    token?<Outlet/>:<Navigate to='/login'/>
   )
 }
 
